e4ee3dd385f83d0ba25334c2e75c691187b86159
Shamil Garifullin <supermariokazan@gmail.com>
2020-10-16 10:28:17 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Movie.java
@@ -0,0 +1,19 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class Movie {
+    private int id;
+    private String name;
+    private String imageUrl;
+    private String genre;
+
+    public Movie(int i, String bladshot, String s, String actionMovie) {
+    }
+}
